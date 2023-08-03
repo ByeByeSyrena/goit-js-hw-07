3009c2478cedf00046dbe4ad5a15c3ab1afce270
Ira S <iryna.shevchenko2@gmail.com>
2023-08-03 20:34:56 +0300

    2nd task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
+
+const ulEl = document.querySelector(".gallery");
+
+const liEls = galleryItems
+    .map((image) => `<li class = "gallery__item">
+    <a
+    class = "gallery__link"
+    href="${image.original}"
+    >
+    <img
+    class="gallery__image"
+    src="${image.preview}"
+    alt="${image.description}">
+    </a>
+    </li>`)
+    .join("");
+
+    ulEl.insertAdjacentHTML("afterbegin", `${liEls}`);
+
+    ulEl.addEventListener("click", onClick);
+
+function onClick(event) {
+    event.preventDefault();
+};
+
+var lightbox = new SimpleLightbox('.gallery a', {
+captionsData: 'alt',
+captionDelay: 250,
+});
